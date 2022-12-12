3d18fc7dd6d45fe297a536252efe0e5504ec8660
Denis <denis.inghelbrecht@outlook.be>
2022-12-12 22:19:21 +0100

    add min and max scale on device-width

--- components/header.js
+++ components/header.js
@@ -12,7 +12,10 @@ export default function Home() {
 					name='description'
 					content='An openAI chat bot created by Denis Inghelbrecht'
 				/>
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1'
+				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<h1>OpenAI Chatbot</h1>
